38375ae3ddd771ce1e89f1e0c5c46704f0d910cb
S. Suzuki <terrierscript@gmail.com>
2022-07-13 23:18:09 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,55 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Center, Stack } from '@chakra-ui/react'
 import Head from 'next/head'
-import React from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+
+
+const playReadySound = (onPlayEnd: () => void) => {
+  const audioCtx = new window.AudioContext()
+  const oscillator = audioCtx.createOscillator()
+  oscillator.type = 'triangle'
+  oscillator.frequency.setValueAtTime(10, audioCtx.currentTime)
+  oscillator.connect(audioCtx.destination)
+
+  oscillator.onended = () => {
+    onPlayEnd()
+  }
+  oscillator.start(0)
+  oscillator.stop(0.1)
+
+}
+const SoundButton = () => {
+  const [ready, setReady] = useState(false)
+
+  const onSetup = () => {
+    playReadySound(() => {
+      setReady(true)
+    })
+  }
+
+  const onPress = () => {
+    const audioCtx = new window.AudioContext()
+
+    const oscillator = audioCtx.createOscillator()
+    oscillator.type = 'triangle'
+    oscillator.frequency.setValueAtTime(400, audioCtx.currentTime)
+    oscillator.frequency.setValueAtTime(200, audioCtx.currentTime + 0.2)
+    oscillator.connect(audioCtx.destination)
+
+    oscillator.start(audioCtx.currentTime)
+    oscillator.stop(audioCtx.currentTime + 0.5)
+
+  }
+  if (!ready) {
+    return <Button onClick={() => onSetup()}>
+      Setup
+    </Button>
+  }
+  return <Stack>
+    <Button onClick={() => { onPress() }}>
+      Bang
+    </Button>
+  </Stack>
+}
 
 export default function Home() {
   return (
@@ -9,6 +58,11 @@ export default function Home() {
         <title></title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Box>
+        <Center flex={1}>
+          <SoundButton />
+        </Center>
+      </Box>
     </Box>
   )
 }
